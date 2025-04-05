3cd9e8eefac2b7cd93ce87a0ae78fdb33e73d117
dobrediana99 <37077941+dobrediana99@users.noreply.github.com>
2025-04-05 11:08:34 +0000

    add solution for Pascals Triangle

--- PrintingPatterns.java
+++ PrintingPatterns.java
@@ -15,7 +15,33 @@ public class PrintingPatterns{
         }
     }
 
+    public static void printPascalsTriangle(int n) {
+        int[][] triangle = new int[n + 1][n + 1];
+
+        // Build the triangle
+        for (int i = 0; i <= n; i++) {
+            triangle[i][0] = triangle[i][i] = 1; // First and last element of each row is 1
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+
+        // Print the triangle in pyramid form
+        for (int i = 0; i <= n; i++) {
+            // Centering spaces
+            for (int s = 0; s < n - i; s++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(triangle[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         PrintingPatterns.printNumberTriangle(5);
+        PrintingPatterns.printNumberTriangle(-12);
+        PrintingPatterns.printPascalsTriangle(3);
     }
 }
